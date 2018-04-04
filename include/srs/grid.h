a139dfdebfbffd4e4f1f1b388c534a87950f7854
Stig Rune Sellevag <stigrs@gmail.com>
2018-04-04 19:40:12 +0200

    Add more methods

--- include/srs/grid.h
+++ include/srs/grid.h
@@ -18,6 +18,7 @@
 #define SRS_GRID_H
 
 #include <srs/types.h>
+#include <gsl/gsl>
 #include <stdexcept>
 #include <string>
 
@@ -46,11 +47,68 @@ public:
         n = 1 + static_cast<size_type>(amax);
     }
 
+    // Generate [amin, amin + 1, .... amax].
+    Grid(double amin, double amax) : a0{amin}, an{amax}, d{1}
+    {
+        n = 1 + static_cast<size_type>(amax - amin);
+    }
+
+    // Generate the list [amin, amin + dd, ..., amax].
+    Grid(double amin, double amax, double dd) : a0{amin}, an{amax}, d{dd}
+    {
+        n = 1 + static_cast<size_type>((amax - amin) / dd);
+    }
+
+    // Initialize data by reading an input file.
+    Grid(std::istream& from, const std::string& key) : a0{0}, an{0}, d{0}, n{0}
+    {
+        set(from, key);
+    }
+
+    // Set new grid data by reading an input file.
+    void set(std::istream& from, const std::string& key);
+
+    // Set new grid data.
+    void set(double amin, double amax, double dd);
+
+    size_type size() const { return n; }
+    double start() const { return a0; }
+    double max() const { return an; }
+    double step() const { return d; }
+    bool empty() const { return n == 0; }
+
+    double operator[](size_type i) const;
+    double operator()(size_type i) const;
+
 private:
     double a0;    // start value
     double an;    // maximum value
     double d;     // step size (common difference)
     size_type n;  // number of steps (size of grid)
 };
+
+inline void Grid::set(double amin, double amax, double dd)
+{
+    a0 = amin;
+    an = amax;
+    d  = dd;
+    n  = 1 + static_cast<size_type>((amax - amin) / dd);
+}
+
+inline double Grid::operator[](size_type i) const
+{
+#ifndef NDEBUG
+    Expects(i < n);
+#endif
+    return a0 + i * d;
+}
+
+inline double Grid::operator()(size_type i) const
+{
+#ifndef NDEBUG
+    Expects(i < n);
+#endif
+    return a0 + i * d;
+}
 
 #endif  // SRS_GRID_H
